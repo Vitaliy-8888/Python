f95cac392e3acd8338625cded177f8f424351c0f
Vitaliy <vladimir42142@mail.ru>
2022-10-01 04:02:17 +0300

    Task4

--- Seminar2/Task4.py
+++ Seminar2/Task4.py
@@ -1,5 +1,26 @@
+# Задача 4. Задать список из N элементов, заполненных числами из [-N, N]. 
+# Найти произведение элементов на указанных позициях. 
+# Позиции хранятся в списке positions - создайте этот список 
+# (например: positions = [1, 3, 6]).
 
-# a = 2
-# b = 3
-# c = a + b
-# print(c)
+number = int(input("Введите число N: ")) 
+
+position1 = int(input("Укажите позицию 1 элемента: ")) 
+position2 = int(input("Укажите позицию 2 элемента: ")) 
+position3 = int(input("Укажите позицию 3 элемента: ")) 
+
+# - в функцию передаются значения:
+# number - размер списка 
+# position1, position2, position3 - позиции указанные пользователем
+def list_numbers(number, position1, position2, position3):  
+    res = []            # - задаем новый список 
+    position  = [position1, position2, position3]   # - в список вносим позиции, которые ввел пользователь
+    result = 1
+    for i in range(-number, number+1):  # - создаем список [-N, N]
+        res.append(i)   # - добавляем элементы в список
+
+    for n in position:   # цикл по позициям
+        result *= res[n]    
+    return print(f'В списке {res}, произведение элементов на позициях {position1}, {position2}, {position3}, = {result}')
+    
+list_numbers(number, position1, position2, position3)
